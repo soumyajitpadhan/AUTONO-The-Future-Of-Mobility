86d8553eb01b99dfa63453ce27c341336474621e
Soumyajit Padhan <soumyajitpadhan15@gmail.com>
2025-01-18 20:05:54 +0530

    replace background image with MP4 video

--- src/Components/Home/Hero.jsx
+++ src/Components/Home/Hero.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Landing_Image from '../../assets/Home/hero.jpg'
+import Autono from '../../assets/Home/autono.mp4'
 const Hero = () => {
   return (
     <div>
-      <div className='relative h-screen bg-cover bg-center font-montserrat' style={{ backgroundImage: `url(${Landing_Image})` }}>
+      <div className='relative h-screen bg-cover bg-center font-montserrat'>
+        <video src={Autono} autoPlay loop muted className='absolute inset-0 w-full h-full object-cover'></video>
         <div className='absolute inset-0 flex justify-center top-32 max-w-md mx-auto sm:top-36 sm:max-w-xl sm:mx-auto md:max-w-3xl md:top-28 lg:top-28 xl:top-32'>
           <div className='text-center'>
             <p className='text-3xl font-light tracking-[.12em] leading-snug sm:text-5xl sm:leading-tight md:text-6xl md:leading-tight'>THE FUTURE OF MOBILITY IS HERE</p>
